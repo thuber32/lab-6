8f63c9f511b065e0102a7ec528b7db7817df740d
Terra Huber <swiftruns26@gmail.com>
2019-07-16 16:02:27 -0500

    Began header and footer functions in js. Added ids to footer

--- js/app.js
+++ js/app.js
@@ -1,31 +1,38 @@
 'use strict';
 
 var hours = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM'];
-var pike = new Store ('Pike',23, 65, 6.3);
-var seaTac = new Store ('SeaTac',3, 24, 1.2);
-var seattle = new Store ('Seattle',11, 38, 3.7);
-var capitol = new Store ('Capitol',20, 38, 2.3);
-var alki = new Store ('Alki',2, 16, 4.6);
-var customersPerHour = [];
-var cookiesPerHour = [];
-var dailyLocationTotal = 0;
-var totalCookiesPerHour = 0;
-var name = name;
 
-function Store(location, minCustomer, maxCustomer, aveCookiePerSale) {
-    this.location = location;
+//var customersPerHour = [];
+//var cookiesPerHour = [];
+//var dailyLocationTotal = 0;
+var totalCookiesPerHour = 0;
+var allStores = [pike, seaTac, seattle, capitol, alki];
+
+//constructor for creating 5 stores
+function Store(minCustomer, maxCustomer, aveCookiePerSale) {
     this.minCustomer = minCustomer;
     this.maxCustomer = maxCustomer;
     this.aveCookiePerSale = aveCookiePerSale;
 }
+//creating stores with min, max and aveCookiePerSale
+var pike = new Store (23, 65, 6.3);
+var seaTac = new Store (3, 24, 1.2);
+var seattle = new Store (11, 38, 3.7);
+var capitol = new Store (20, 38, 2.3);
+var alki = new Store (2, 16, 4.6);
 
+//constructor to add the customers per hour to the store
 Store.prototype.customersPerHour() {
+    var customersPerHour = [];
     for (var i = 0; i < hours.length; i++) {
         this.customersPerHour.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer);
     }
 }
 
+//constructor to add the cookies per hour to the store
 Store.prototype.calCookiesPerHour() {
+    var cookiesPerHour = [];
+    var dailyLocationTotal= 0;
     for (var i = 0; i < hours.length; i++) {
         var hourlyCookies = Math.ceil(this.customersPerHour[i] * this.aveCookiePerSale); //need to round up cookies
         this.cookiesPerHour.push(hourlyCookies);
@@ -33,10 +40,11 @@ Store.prototype.calCookiesPerHour() {
     }
 }
 
+//constructor to run the two methods and append informtion to location
 Store.prototype.render() {
     this.calCustomersPerHour();
     this.calCookiesPerHour();
-    var tdEl = document.getElementById('pike');
+    var tdEl = document.getElementById('');
     for (var i = 0; i < hours.length; i++) {
         var tdElNew = document.createElement('td');
         tdElNew.textContent = this.cookiesPerHour[i] + ' cookies';
@@ -44,10 +52,29 @@ Store.prototype.render() {
     }
 }
 
+//function for placing time into the header of the table
 var header = function () {
-
+    var theadEl = document.getElementsById('time');
+    for (var i = 0; i < hours.length; i++){
+        var thEl = document.createElement ('th');
+        thEl.textContent = hours[i];
+    }
 }
 
+//fuction for placing the total number of cookies solde each hour AND total number of cookies sold overall
+var footer = function(){
+    var tfooterEl= document.getElementById('totals');
+    this.Store.cookiesPerHour[j],
+    for(var j = 0; i < allStores.length; j++){
+       totalCookiesPerHour += 
+    }
+}
+
+(function renderAllStores() {
+    for (var i = 0; i < allStores.length; i++) {
+        allStores[i].render();
+    }
+})();
 
 
 /*
